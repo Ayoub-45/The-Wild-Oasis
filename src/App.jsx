1156b577c030911d2cfccaf5241283b2753cfe85
Ayoub-45 <ayoubnasraoui161@yahoo.fr>
2023-10-26 07:12:44 +0100

    Explore styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
+import styled from "styled-components"
+const Button=styled.button`
+  color: white;
+  background-color: brown;
+  padding:1.5rem;
+  font-size: 30px;
+  border:none;
+  border-radius: 2rem;
+  
+`
+const StyledApp=styled.div`
+background-color: beige;
+padding:0.5rem;
+width:50%;
+height:50%;
+margin:auto;
+`;
+const H1=styled.h1`
+font-size:32px
+`
+
+
 export default function App(){
-  return <div>Hello world</div>
+  return (
+  <StyledApp>
+  <H1>Hello</H1>
+  <Button onClick={()=>alert("check out")}>Check out</Button>
+  </StyledApp>)
 }
